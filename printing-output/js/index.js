a5430412d27ef020de02ce13756ad2da359baeb2
bist0002 <105647883+bist0002@users.noreply.github.com>
2022-05-18 12:41:09 -0400

    A01 file added.

---
+++ printing-output/js/index.js
@@ -0,0 +1,41 @@
+
+//task-01
+let a = 3;
+let b = 5;
+let c = null;
+
+let re = "Task-01"+ '\n';
+re += "--------------------------"+ '\n';
+re += 'let a = 3;\n';
+re += 'let b = ;\n';
+re += 'let c;\n';
+re += '------------\n';
+re +='a + b = ' + (a+b) + '\n';
+re += 'a - b = ' + (a-b) + '\n';
+re += 'a / b = ' + (a/b) +'\n';
+re += 'a % b = ' + (a%b) +'\n';
+re += 'a += b' + ": " + (a+=b) +'\n';
+re += 'a -= b: ' + (a-=b) +'\n';
+re += 'a *= b: ' + (a*=b) +'\n';
+re += 'a /= b: ' + (a/=b) + '\n';
+re += 'a %= b: ' + (a%=b) + '\n';
+re += 'a == b: ' +(a==b) +'\n';
+re += 'a != b: ' + (a!=b) +'\n';
+re += 'a > b: ' + (a>b) +'\n';
+re += 'a < b: ' + (a<b) +'\n';
+re += '!a && !c: ' + (!a&&!c) +'\n';
+re += '!a || !C: ' + (!a||!c) +'\n';
+alert(re);
+
+
+//task-02
+let fname= 'Ashray';
+let lname= 'Bista';
+let email= 'ashraybista@email.com';
+
+let output = "Task-02"+ '\n';
+output += "--------------------------"+ '\n';
+output += '\"My name is ' + fname + ' ' + lname + '.' + ' ' + 'You can contact me at ' + email + '\"';
+alert(output);
+
+
